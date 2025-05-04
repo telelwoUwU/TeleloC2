b59a05ac70a1f7636037bd1cc897c65ea98d64a4
telelwoUwU <telelo@outlook.hu>
2025-05-04 11:56:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.cnc import start as start_cnc
 if __name__ == '__main__':
     try:
         start_cnc()
-    except:
-        print("Error, skipping.")
+    except Exeption as e:
+        print("Error, skipping.{e}")
